6bb181272b9f75b8b381cc0f0cd8f71ba9175a51
Jason Covey <jcovey@indium.com>
2025-07-23 15:15:51 -0400

    added test application

---
+++ DotNetDaterbaser.TestApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetDaterbaser.TestApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
